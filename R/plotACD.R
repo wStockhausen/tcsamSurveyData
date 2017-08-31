6515265d313a13ba039a9c73eb019ed832e6f805
wStockhausen <william.stockhausen@noaa.gov>
2017-08-31 15:24:54 -0700

    changes to plotACD

--- R/plotACD.R
+++ R/plotACD.R
@@ -7,6 +7,9 @@
 #'@param type - "abundance" or "biomass"
 #'@param factors - column names to use as factors for plots
 #'@param faceting - faceting formula
+#'@param facetsScale - value for \code{scale} parameter passed to \code{facet_grid()} ("fixed","free_x","free_y","free")
+#'@param facetsDrop - value for \code{drop} parameter passed to \code{facet_grid()} (drop missing facet levels)
+#'@param xlab - x-axis label ("year")
 #'@param ylab - y-axis label
 #'@param ci - confidence interval for error bars (e.g., 0.95)
 #'@param ci.type - confidence interval type ('normal', 'lognormal')
@@ -14,8 +17,6 @@
 #'@param ylims - y-axis limits (calculated internally if NULL)
 #'@param ggtheme - ggplot2 theme
 #'@param jitter - flag to jitter points
-#'@param ncol - number of columns of plots per page
-#'@param nrow - number of rows of plots per page
 #'@param showPlots - flag to show plots immediately
 #'@param verbosity - flag (>0) to print diagnostic output
 #'
@@ -35,7 +36,10 @@
 plotACD<-function(acd,
                   type=c("abundance","biomass"),
 				  factors=NULL,
-				  faceting="stratum",
+				  faceting=NULL,
+				  facetsScale="fixed",
+				  facetsDrop=FALSE,
+				  xlab="year",
                   ylab=NULL,
                   ci=0.8,
                   ci.type='normal',
@@ -43,22 +47,26 @@ plotACD<-function(acd,
                   ylims=NULL,
                   ggtheme=theme_grey(),
                   jitter=TRUE,
-                  ncol=2,
-                  nrow=5,
                   showPlots=TRUE,
                   verbosity=0){
 
     nf<-0
-    if (!is.null(factors)) nf<-length(factors); ##number of "factors", e.g. sex, maturity, shell_condition
+    if (!is.null(factors)) {
+        factors<-tolower(factors);
+        nf<-length(factors); ##number of "factors", e.g. sex, maturity, shell_condition
+    }
 
     dfr<-acd;
-    names(dfr)<-tolower(names(acd));
+    names(dfr)<-tolower(names(dfr));
+
     dfr$value<-dfr[[paste0("tot",tolower(type[1]))]];
     dfr$cv   <-dfr[[paste0("cv",tolower(type[1]))]];
+
     if (is.null(ylab)){
         if (tolower(type[1])=='abundance') ylab<-"Abundance (millions)";
         if (tolower(type[1])=='biomass')   ylab<-"Biomass (1000's t)";
     }
+
     if (tolower(ci.type)=='normal'){
         stdv<-dfr$cv*dfr$value;
         dfr$lci<-qnorm((1-ci)/2,mean=dfr$value,sd=stdv);
@@ -75,19 +83,16 @@ plotACD<-function(acd,
         dfr <- dfr[idx,];
     }
 
-    mxp<-nrow*ncol;
-    strata<-unique(dfr$stratum);
-    npg<-ceiling(length(strata)/mxp);
-
-    if (is.null(ylims)) ylims<-c(min(0,min(dfr$uci,na.rm=TRUE)),max(dfr$uci,na.rm=TRUE));
-    if (verbosity>0) cat("ylims = ",ylims,'\n')
+    if (is.null(ylims)&is.null(facetsScale)) {
+        ylims<-c(min(0,min(dfr$uci,na.rm=TRUE)),max(dfr$uci,na.rm=TRUE));
+        if (verbosity>0) cat("ylims = ",ylims,'\n')
+    }
     ##cat("jitter =",jitter,"\n")
 
     fax<-'black';
     dfr$yrp <- dfr$year;
     if (nf>0) {
-        facs<-factors;
-        fax<-do.call(paste,c(dfr[tolower(facs)],sep=', '))
+        fax<-do.call(paste,c(dfr[factors],sep=', '));
         dfr$fax<-fax;
         ulevs<-unique(fax);
         nlevs<-length(ulevs);
@@ -100,14 +105,16 @@ plotACD<-function(acd,
 
         p <- ggplot(data=dfr);
         p <- p + geom_line(aes(x=yrp,y=value,colour=fax),size=1);
-        p <- p + geom_errorbar(aes(x=yrp,y=value,ymin=lci,ymax=uci,colour=fax));
+        p <- p + geom_errorbar(aes(x=yrp,ymin=lci,ymax=uci,colour=fax));
         p <- p + geom_point(aes(x=yrp,y=value,color=fax,shape=fax),size=2);
         p <- p + scale_x_continuous();
-        p <- p + scale_y_continuous(breaks=pretty(ylims),limits=ylims,expand=c(0.01,0),oob=squish);
+        if (!is.null(ylims)){
+            p <- p + scale_y_continuous(breaks=pretty(ylims),limits=ylims,expand=c(0.01,0),oob=squish);
+        }
 #        p <- p + position_jitter()
         p <- p + geom_hline(yintercept=0,colour='black',size=0.5);
-        p <- p + labs(x="Year",y=ylab)
-        if (!is.null(faceting)) p <- p + facet_grid(faceting);
+        p <- p + labs(x=xlab,y=ylab)
+        if (!is.null(faceting)) p <- p + facet_grid(tolower(faceting),scales=facetsScale,drop=facetsDrop);
         p <- p + guides(fill=guide_legend(''),colour=guide_legend(''),shape=guide_legend(''));
         p <- p + ggtheme;
         if (showPlots) print(p);
